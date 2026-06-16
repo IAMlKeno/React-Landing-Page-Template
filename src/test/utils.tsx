e7a2cc0090e46505fe2455dfb98f890dc975b168
Elkeno Jones <elkeno.jones@binarystar.ca>
2026-06-15 23:54:16 -0300

    manually copy over test setup files

---
+++ src/test/utils.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, type RenderOptions } from '@testing-library/react';
+import { CartClassProvider } from '../features/cart/context/CartClassProvider';
+
+const AllProviders = ({ children }: { children: React.ReactNode }) => (
+  <CartClassProvider>{children}</CartClassProvider>
+);
+
+/**
+ * Use instead of @testing-library/react `render` for components that
+ * depend on CartClassContext (CartIcon, CartModal, ProductList, Navigation).
+ */
+const renderWithProviders = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: AllProviders, ...options });
+
+export * from '@testing-library/react';
+export { renderWithProviders };
